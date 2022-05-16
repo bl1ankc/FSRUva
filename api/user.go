a20cc5d9333f07ad356b5580f3a1e26b9c6f3ec9
UnknownX <1791099167@qq.com>
2022-05-16 23:56:22 +0800

    2022/5/16图片暂时不保存到本地

--- api/user.go
+++ api/user.go
@@ -148,10 +148,12 @@ func UploadImg(c *gin.Context) bool {
 
 	//上传失败
 	if file != nil {
+		/* 保存到本地
 		if err := c.SaveUploadedFile(file, "./img/"+filename); err != nil {
 			//c.JSON(500, gin.H{"code": 500, "desc": "保存图片失败"})
 			return false
 		}
+		*/
 	} else {
 		//c.JSON(400, gin.H{"code": 400, "desc": "未上传图片"})
 		return false
